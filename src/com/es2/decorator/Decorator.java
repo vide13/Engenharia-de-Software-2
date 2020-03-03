4c7a02901d6ba3b464677e8f2087ab27ecac4613
Rvide <ruivide13@hotmail.com>
2020-03-03 20:53:16 +0000

    Fix Decorator init

--- src/com/es2/decorator/Decorator.java
+++ src/com/es2/decorator/Decorator.java
@@ -4,8 +4,10 @@ import java.io.IOException;
 
 public class Decorator implements AuthInterface {
 
+    AuthInterface auth;
 
     public Decorator(AuthInterface auth) {
+        auth = new Auth();
     }
 
     public void auth(String username, String password) throws AuthException, IOException {
