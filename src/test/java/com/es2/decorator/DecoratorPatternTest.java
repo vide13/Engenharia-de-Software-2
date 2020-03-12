0e985c9f7a4437934873385c910069810bc57d55
Rvide <ruivide13@hotmail.com>
2020-03-12 15:40:53 +0000

    Code Style Fixes

--- src/test/java/com/es2/decorator/DecoratorPatternTest.java
+++ src/test/java/com/es2/decorator/DecoratorPatternTest.java
@@ -1,6 +1,10 @@
 package com.es2.decorator;
 
-import org.junit.jupiter.api.*;
+import org.junit.Test;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -9,7 +13,7 @@ import java.io.PrintStream;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-class DecoratorPatternTest {
+public class DecoratorPatternTest {
     private static final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
 
     @BeforeAll public static void setUpStreams() {
@@ -20,38 +24,34 @@ class DecoratorPatternTest {
         System.setOut(System.out);
     }
 
-    @BeforeEach void setUp() throws Exception {
-    }
-
     @DisplayName("Test if authentication is performed correctly using admin_user/admin_password")
-    @Test void testRightAuthWithoutDecorators() throws AuthException, IOException {
+    @Test public void testRightAuthWithoutDecorators() throws AuthException, IOException {
         AuthInterface auth = new Auth();
         auth.auth("admin_user", "admin_password");
     }
 
     @DisplayName("Test if authentication is exception is thrown for bad authentication") @Test
-    void testWrongAuthWithoutDecorators() throws AuthException, IOException {
+    public void testWrongAuthWithoutDecorators() {
         AuthInterface auth = new Auth();
-        assertThrows(AuthException.class, () -> {
-            auth.auth("admin_user1", "admin_password1");
-        });
+        assertThrows(AuthException.class, () -> auth.auth("admin_user1", "admin_password1"));
     }
 
     @DisplayName("Test if Logging is decorating correctly the authentication") @Test
-    void testLoggingDecorator() throws AuthException, IOException {
+    public void testLoggingDecorator() throws AuthException, IOException {
         AuthInterface auth = new Logging(new Auth());
         auth.auth("admin_user", "admin_password");
         assertTrue(outContent.toString().contains("auth()"));
     }
 
     @DisplayName("Test if CommonWordsValidator is decorating correctly the authentication") @Test
-    void testCommonWordsValidatorDecorator() throws AuthException, IOException {
+    public void testCommonWordsValidatorDecorator() throws AuthException, IOException {
         AuthInterface auth = new CommonWordsValidator(new Auth());
         auth.auth("admin_user", "admin_password");
     }
 
     @DisplayName("Test if CommonWordsValidator exception is thrown if the password is common") @Test
-    void testCommonWordsValidatorDecoratorCommonPassword() throws AuthException, IOException {
+    public void testCommonWordsValidatorDecoratorCommonPassword()
+        throws AuthException, IOException {
         AuthInterface auth = new CommonWordsValidator(new Auth());
         assertThrows(AuthException.class, () -> {
             auth.auth("admin_user", "bird");
@@ -59,7 +59,7 @@ class DecoratorPatternTest {
     }
 
     @DisplayName("Test if Logging and CommonWordsValidator is decorating correctly the authentication")
-    @Test void testLoggingCommonWordsValidatorDecorator() throws AuthException, IOException {
+    @Test public void testLoggingCommonWordsValidatorDecorator() throws AuthException, IOException {
         AuthInterface auth = new CommonWordsValidator(new Logging(new Auth()));
 
         auth.auth("admin_user", "admin_password");
